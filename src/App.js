1302243a5213ad9ec2c241f23f7d1d534456d152
itsmihir <inmihir11@gmail.com>
2021-11-01 11:56:32 +0530

    react lazy added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import { Switch, Route, Redirect } from "react-router-dom";
-import { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import AuthContext from "./Store/Auth";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Voice from "./Pages/Voice";
-import Command from "./Pages/Command";
-import Bot from "./Pages/Bot";
+import { Spinner } from "react-bootstrap";
+
+const Home = React.lazy(() => import("./Pages/Home"));
+const Voice = React.lazy(() => import("./Pages/Voice"));
+const Command = React.lazy(() => import("./Pages/Command"));
+const Login = React.lazy(() => import("./Pages/Login"));
+const Bot = React.lazy(() => import("./Pages/Bot"));
 
 function App() {
     const userName = useContext(AuthContext).userName;
     return (
-        <Switch>
-            <Route path="/login">
-                {userName && <Redirect to="/" />}
-                <Login />
-            </Route>
+        <Suspense
+            fallback={
+                <div style={{ marginTop: "40vh", textAlign: "center" }}>
+                    <Spinner animation="border" />
+                </div>
+            }
+        >
+            <Switch>
+                <Route path="/login">
+                    {userName && <Redirect to="/" />}
+                    <Login />
+                </Route>
 
-            {!userName && <Redirect to="/login" />}
+                {!userName && <Redirect to="/login" />}
 
-            <Route path="/" exact>
-                <Home />
-            </Route>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
 
-            <Route path="/voice">
-                <Voice />
-            </Route>
+                <Route path="/voice">
+                    <Voice />
+                </Route>
 
-            <Route path="/command">
-                <Command />
-            </Route>
+                <Route path="/command">
+                    <Command />
+                </Route>
 
-            <Route path="/bot">
-                <Bot />
-            </Route>
-        </Switch>
+                <Route path="/bot">
+                    <Bot />
+                </Route>
+            </Switch>
+        </Suspense>
     );
 }
 
